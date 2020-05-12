0fb25f7e64cbd13dd6bd490c240b5954852ae5e0
mike spindel <mike@spindel.is>
2020-05-12 16:16:11 -0700

    Incorporate README with setup.py and LICENSE

--- setup.py
+++ setup.py
@@ -1,30 +1,33 @@
 from setuptools import setup
 
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 
 setup(
     name="tracenl",
     version="0.1",
     author="Mike Spindel",
     author_email="mike@spindel.is",
-    license="MIT",
+    description="Easily snoop on netlink messages with ptrace",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/deactivated/python-tracenl",
     keywords="netlink capture trace",
-    url="http://github.com/deactivated/python-nltrace",
-    description=".",
+    license="MIT",
     packages=["tracenl"],
-    install_requires=[
-        "pyroute2",
-        "python-ptrace"
-    ],
+    install_requires=["pyroute2", "python-ptrace"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
         "Natural Language :: English",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux"
     ],
+    python_requires='>=3.6',
     entry_points={"console_scripts": ["tracenl=tracenl.__main__:main"]},
 )
